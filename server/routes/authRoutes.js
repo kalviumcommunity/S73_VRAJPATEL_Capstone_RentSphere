9728962a25f7e20ffe206e2a463e558fef71ce02
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-03-22 21:33:20 +0530

    Added a PUT endpoint

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { signup,getUser,getAllUsers } from "../controllers/authController.js";
+import { signup,getUser,getAllUsers,updateUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.get("/user/:id", getUser);
 router.get("/users", getAllUsers); 
+router.put("/user/:id",updateUser);
 
 
 export default router;
