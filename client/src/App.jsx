d28998d130eedaf0b887a8294793d840484876eb
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-03-22 16:08:13 +0530

    Bug fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 
 
 const App = () => {
